4cfb9114464602cefe69dd1d1c28bccb846790f0
Ioana <cheresioana@gmail.com>
2021-05-26 22:15:31 +0300

    added comments

--- src/main/java/Send.java
+++ src/main/java/Send.java
@@ -17,11 +17,16 @@ public class Send {
 
     public static void main(String[] argv) throws Exception {
 
+        // Connecting to cloud using url
         ConnectionFactory factory = new ConnectionFactory();
         factory.setUri("amqps://mendpuhm:Ny0KzCPeb7NaQTn3GXsJxp8S8PmSuz7A@hawk.rmq.cloudamqp.com/mendpuhm");
+
         try {
+            //attribute id to client
             int id = Integer.parseInt(argv[0]);
             System.out.println("Started client with id: [ " + id + " ]");
+
+            //connect to messaging queue
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
             channel.queueDeclare(QUEUE_MESSAGE, true, false, false, null);
@@ -30,16 +35,20 @@ public class Send {
                 String message = new String(delivery.getBody(), "UTF-8");
                 ObjectMapper mapper = new ObjectMapper();
                 CustomData data = mapper.readValue(message, CustomData.class);
+                //check if the current process should read the message (check if it's id is in destinatari)
                 if (data.getDestinatar().contains("" + id)){
                     System.out.println(" [x] Received '" + message + "'");
                     data.setDestinatar(data.getDestinatar().replace("" + id, ""));
                     if (!data.getDestinatar().trim().isEmpty()){
+                        //if there are still processes that didn't receive the message re-add it to the queue
                         System.out.println("Inca mai sunt destinatari care nu au primit mesajul: " + data.getDestinatar());
                         channel.basicPublish("", QUEUE_MESSAGE, null, data.toString().getBytes());
                     }
                 }
                 else{
+                    //if the process should not read the message or if the message if for broadcast re-add it into the queue
                     channel.basicPublish("", QUEUE_MESSAGE, null, message.getBytes());
+                    //check if the message is broadcast
                     if (data.getDestinatar().contains("-1")){
                         System.out.println(" [x] Received '" + message + "'");
                     }
@@ -48,30 +57,21 @@ public class Send {
             };
             channel.basicConsume(QUEUE_MESSAGE, true, deliverCallback, consumerTag -> { });
 
+            //read from keyboard the processes to whom to send the message
             Scanner scan= new Scanner(System.in);
             while(true) {
                 System.out.println("Dati id-ul celor carora vreti sa le transmiteti mesajul. Pentru broadcast scrieti -1");
                 String text = scan.nextLine();
+                //create the message
                 CustomData data = CustomData.createInstance(id,text, "You have been messaged by " + id);
+                //publish the message
                 channel.basicPublish("", QUEUE_MESSAGE, null, data.toString().getBytes());
             }
-            /*while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-                String[] split = data.split("\t\t");
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                LocalDateTime start = LocalDateTime.parse(split[0], formatter);
-                LocalDateTime end = LocalDateTime.parse(split[1], formatter);
-
-
-                channel.basicPublish("", QUEUE_NAME, null, message.toString().getBytes());
-                System.out.println(" [x] Sent '" + message + "'");
-                Thread.sleep(1000);
-            }*/
 
         }
         catch (Exception e){
-            System.out.println("Exception");
+            System.out.println("Exception encountered");
+
         }
     }
 }
